278ab4a850ae0cf9b32c302ed80aa9668e25414c
Mitesh Jethva <miteshjethva28@gmail.com>
2023-10-02 16:30:08 +0530

    Data Transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,11 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -26,7 +29,7 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
 
-            logging.info("Train test spllit initiated")
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size = 0.2, random_state = 42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
@@ -44,4 +47,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
